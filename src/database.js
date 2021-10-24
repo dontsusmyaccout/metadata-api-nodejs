8cc60cd8a9fa3aa9b47e26ae6fe63c57a70c276d
BNB Punks <bnbpunksdev@gmail.com>
2021-10-24 16:24:00 -0700

    update data

--- src/database.js
+++ src/database.js
@@ -1,22 +1,352 @@
 module.exports = {
   "1": {
-    "name": "Chunky Monkey",
-    "birthday": "10-18"
+    "background": "The Clown",
+    "skull": "Green",
+    "eyes": "Green",
+    "role": "Designer",
+    "hair": "Yellow"
   },
   "2": {
-    "name": "Flerp Derp",
-    "birthday": "01-08"
+    "background": "Gypsy Ball",
+    "skull": "Red",
+    "eyes": "Brown",
+    "role": "Realtor",
+    "hair": "Gold"
   },
   "3": {
-    "name": "Buttle McFuddle",
-    "birthday": "01-22"
+    "background": "Lucky Man",
+    "skull": "Pink",
+    "eyes": "Green",
+    "role": "Builder",
+    "hair": "Red"
   },
   "4": {
-    "name": "Quiero Pancakes",
-    "birthday": "03-04"
+    "background": "Hot Smoke and Sasafrass",
+    "skull": "Red",
+    "eyes": "Blue",
+    "role": "Designer",
+    "hair": "Turquoise"
   },
   "5": {
-    "name": "Guino the Penguin",
-    "birthday": "03-09"
+    "background": "Sam",
+    "skull": "Red",
+    "eyes": "Blue",
+    "role": "Designer",
+    "hair": "Pink"
+  },
+  "6": {
+    "background": "God Doge",
+    "skull": "Pink",
+    "eyes": "Ethereum",
+    "role": "Realtor",
+    "hair": "Red"
+  },
+  "7": {
+    "background": "Sam",
+    "skull": "Green",
+    "eyes": "Bitcoin",
+    "role": "Designer",
+    "hair": "Green"
+  },
+  "8": {
+    "background": "Moon In June",
+    "skull": "Turquoise",
+    "eyes": "Brown",
+    "role": "Designer",
+    "hair": "Yellow"
+  },
+  "9": {
+    "background": "Love Without Sound",
+    "skull": "Purple",
+    "eyes": "Ethereum",
+    "role": "King",
+    "hair": "Orange"
+  },
+  "10": {
+    "background": "Sam",
+    "skull": "Red",
+    "eyes": "Brown",
+    "role": "Designer",
+    "hair": "Yellow"
+  },
+  "11": {
+    "background": "Playing With Magic",
+    "skull": "Pink",
+    "eyes": "Blue",
+    "role": "King",
+    "hair": "Red"
+  },
+  "12": {
+    "background": "Peace by Peace",
+    "skull": "Pink",
+    "eyes": "Bitcoin",
+    "role": "Realtor",
+    "hair": "Yellow"
+  },
+  "13": {
+    "background": "A Salty Dog",
+    "skull": "Pink",
+    "eyes": "Ethereum",
+    "role": "Designer",
+    "hair": "Orange"
+  },
+  "14": {
+    "background": "Free Advice",
+    "skull": "Green",
+    "eyes": "Green",
+    "role": "Lawyer",
+    "hair": "Green"
+  },
+  "15": {
+    "background": "Hot Smoke and Sasafrass",
+    "skull": "Turquoise",
+    "eyes": "Blue",
+    "role": "King",
+    "hair": "Gold"
+  },
+  "16": {
+    "background": "Moon In June",
+    "skull": "Turquoise",
+    "eyes": "Green",
+    "role": "Builder",
+    "hair": "Orange"
+  },
+  "17": {
+    "background": "The Clown",
+    "skull": "Turquoise",
+    "eyes": "Green",
+    "role": "Builder",
+    "hair": "Gold"
+  },
+  "18": {
+    "background": "Hot Smoke and Sasafrass",
+    "skull": "Green",
+    "eyes": "Ethereum",
+    "role": "Judge",
+    "hair": "Red"
+  },
+  "19": {
+    "background": "Hot Smoke and Sasafrass",
+    "skull": "Pink",
+    "eyes": "Green",
+    "role": "Realtor",
+    "hair": "Yellow"
+  },
+  "20": {
+    "background": "Gypsy Ball",
+    "skull": "Purple",
+    "eyes": "Green",
+    "role": "Builder",
+    "hair": "Gold"
+  },
+  "21": {
+    "background": "Lucky Man",
+    "skull": "Purple",
+    "eyes": "Green",
+    "role": "Judge",
+    "hair": "Yellow"
+  },
+  "22": {
+    "background": "Bus Stop",
+    "skull": "Pink",
+    "eyes": "Blue",
+    "role": "Designer",
+    "hair": "Gold"
+  },
+  "23": {
+    "background": "Hippie Wallpaper",
+    "skull": "Purple",
+    "eyes": "Bitcoin",
+    "role": "King",
+    "hair": "Pink"
+  },
+  "24": {
+    "background": "Curse Of The Witches",
+    "skull": "Turquoise",
+    "eyes": "Bitcoin",
+    "role": "Realtor",
+    "hair": "Yellow"
+  },
+  "25": {
+    "background": "Big Jobs",
+    "skull": "Pink",
+    "eyes": "Green",
+    "role": "Designer",
+    "hair": "Gold"
+  },
+  "26": {
+    "background": "The Mole",
+    "skull": "Pink",
+    "eyes": "Bitcoin",
+    "role": "King",
+    "hair": "Pink"
+  },
+  "27": {
+    "background": "Curse Of The Witches",
+    "skull": "Turquoise",
+    "eyes": "Brown",
+    "role": "Builder",
+    "hair": "Pink"
+  },
+  "28": {
+    "background": "Gypsy Ball",
+    "skull": "Pink",
+    "eyes": "Ethereum",
+    "role": "King",
+    "hair": "Red"
+  },
+  "29": {
+    "background": "The Clown",
+    "skull": "Green",
+    "eyes": "Ethereum",
+    "role": "Builder",
+    "hair": "Orange"
+  },
+  "30": {
+    "background": "Peace by Peace",
+    "skull": "Purple",
+    "eyes": "Bitcoin",
+    "role": "Judge",
+    "hair": "Pink"
+  },
+  "31": {
+    "background": "Circles",
+    "skull": "Purple",
+    "eyes": "Bitcoin",
+    "role": "Judge",
+    "hair": "Yellow"
+  },
+  "32": {
+    "background": "Gypsy Ball",
+    "skull": "Pink",
+    "eyes": "Bitcoin",
+    "role": "Judge",
+    "hair": "Green"
+  },
+  "33": {
+    "background": "Love Without Sound",
+    "skull": "Red",
+    "eyes": "Blue",
+    "role": "Builder",
+    "hair": "Green"
+  },
+  "34": {
+    "background": "Hot Smoke and Sasafrass",
+    "skull": "Purple",
+    "eyes": "Ethereum",
+    "role": "King",
+    "hair": "Green"
+  },
+  "35": {
+    "background": "Hippie Wallpaper",
+    "skull": "Green",
+    "eyes": "Blue",
+    "role": "Builder",
+    "hair": "Turquoise"
+  },
+  "36": {
+    "background": "Gypsy Ball",
+    "skull": "Pink",
+    "eyes": "Ethereum",
+    "role": "Realtor",
+    "hair": "Turquoise"
+  },
+  "37": {
+    "background": "Lucifer",
+    "skull": "Pink",
+    "eyes": "Green",
+    "role": "Lawyer",
+    "hair": "Green"
+  },
+  "38": {
+    "background": "Gypsy Ball",
+    "skull": "Pink",
+    "eyes": "Ethereum",
+    "role": "Realtor",
+    "hair": "Gold"
+  },
+  "39": {
+    "background": "Gypsy Ball",
+    "skull": "Red",
+    "eyes": "Bitcoin",
+    "role": "Realtor",
+    "hair": "Gold"
+  },
+  "40": {
+    "background": "Lucifer",
+    "skull": "Turquoise",
+    "eyes": "Brown",
+    "role": "Lawyer",
+    "hair": "Red"
+  },
+  "41": {
+    "background": "Kingdom of Heaven",
+    "skull": "Green",
+    "eyes": "Bitcoin",
+    "role": "King",
+    "hair": "Yellow"
+  },
+  "42": {
+    "background": "Curse Of The Witches",
+    "skull": "Purple",
+    "eyes": "Ethereum",
+    "role": "Builder",
+    "hair": "Gold"
+  },
+  "43": {
+    "background": "The Mole",
+    "skull": "Pink",
+    "eyes": "Ethereum",
+    "role": "Designer",
+    "hair": "Turquoise"
+  },
+  "44": {
+    "background": "Free Advice",
+    "skull": "Purple",
+    "eyes": "Brown",
+    "role": "Designer",
+    "hair": "Orange"
+  },
+  "45": {
+    "background": "A Salty Dog",
+    "skull": "Purple",
+    "eyes": "Bitcoin",
+    "role": "King",
+    "hair": "Turquoise"
+  },
+  "46": {
+    "background": "Gypsy Ball",
+    "skull": "Turquoise",
+    "eyes": "Brown",
+    "role": "Judge",
+    "hair": "Yellow"
+  },
+  "47": {
+    "background": "Peace by Peace",
+    "skull": "Pink",
+    "eyes": "Brown",
+    "role": "Realtor",
+    "hair": "Gold"
+  },
+  "48": {
+    "background": "Circles",
+    "skull": "Green",
+    "eyes": "Green",
+    "role": "Realtor",
+    "hair": "Gold"
+  },
+  "49": {
+    "background": "Hippie Wallpaper",
+    "skull": "Pink",
+    "eyes": "Green",
+    "role": "Lawyer",
+    "hair": "Turquoise"
+  },
+  "50": {
+    "background": "Lucky Man",
+    "skull": "Purple",
+    "eyes": "Bitcoin",
+    "role": "Designer",
+    "hair": "Green"
   }
 }
